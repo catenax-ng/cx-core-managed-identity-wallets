e870b2ac1e22f17c9702c04e91bc5d8f02f0f5a0
turfa51nodes <turfa@51nodes.io>
2022-08-15 10:28:48 +0200

    improve script and readme to generate DID

--- dev-assets/generate-did-from-seed/generateDidFromSeed.sh
+++ dev-assets/generate-did-from-seed/generateDidFromSeed.sh
@@ -2,14 +2,8 @@
 
 echo "Start the Docker container" 
 docker-compose up -d
-echo "Please open a new terminal and run the follwoing command:" 
-echo ""
-echo "   docker logs acapy_container -f "
-echo ""
-### Sleep for 180 seconds ###
-sleep 180
-
+sleep 20
+docker logs acapy_container -f
 echo "Docker container is stopping"
 docker-compose down -v
 echo "Docker container stopped and has been removed"
- 
